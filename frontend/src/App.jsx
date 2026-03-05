b0d8753f22842b60c588d139e289644a955a722e
Aman Pratap Singh <amanpratapsingh030@gmail.com>
2026-03-05 16:07:32 +0530

    Added Reacy-Query for API fetching.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import './App.css'
+import { PingComponent } from './components/atoms/pingComponent.jsx';
+
+// import { pingApi } from './apis/ping.js';
+// import usePing from './hooks/apis/queries/usePing.jd';
 
 function App() {
+
+  // useEffect(()=> {
+  //   pingApi();
+  // }, []);
+
+  // const { isLoading, data } = usePing();
+
+  // if(isLoading) {
+  //   return( 
+  //     <>
+  //     Loading.......</>
+
+  //   )
+  // }
+
+  const [isVisible, setIsVisible] = useState(false);
+
   return (
     <>
-      Hello Aman!!!
+      <button onClick={() => setIsVisible(!isVisible)}> Toggle </button>
+      {isVisible && <PingComponent />}
+
     </>
   );
 }
